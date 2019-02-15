260467ec06b3bbe46a745b97d43f84b8edba2664
Daniel Munch <30357821+Dmunch04@users.noreply.github.com>
2019-02-15 19:28:05 +0100

    Updated example.py

--- example.py
+++ example.py
@@ -8,3 +8,24 @@ class ExampleFunctions (object):
     self.obj2 = obj2
     self.obj3 = obj3
     self.obj4 = obj4
+
+  def __str__ (self):
+    return '%s: %s: %s: %s:' % (self.obj1, self.obj2, self.obj3, self.obj4)
+  
+def returnResult (arg1, arg2, arg3, arg4):
+  result = []
+  
+  r = ExampleFunctions(int(arg1), int(arg2), int(arg3), int(arg4))
+  
+  result.append(r)
+  
+  return result
+
+def getResult (arg1):
+  if not arg1 > = 0:
+    return
+  
+  returnResult(int(arg1), int(arg1 + 1), int(arg1 + 2), int(arg1 + 3))
+  
+def get (arg):
+  getResult(arg)
