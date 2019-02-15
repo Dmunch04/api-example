46cc6dd4616b1e5eb68d17c1fd03f5c551b75b36
Daniel Munch <30357821+Dmunch04@users.noreply.github.com>
2019-02-15 18:50:06 +0100

    Added example file and content (Only started)

---
+++ example.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import json
+
+class ExampleFunctions (object):
+  def __init__ (self, obj1, obj2, obj3, obj4):
+    self.obj1 = obj1
+    self.obj2 = obj2
+    self.obj3 = obj3
+    self.obj4 = obj4
