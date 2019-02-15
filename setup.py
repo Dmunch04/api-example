a48c7430fb8d61017a7c13502c5a5b077a8ec5e6
Daniel Munch <30357821+Dmunch04@users.noreply.github.com>
2019-02-15 18:47:49 +0100

    Added setup.py and content

---
+++ setup.py
@@ -0,0 +1,21 @@
+import multiprocessing
+from setuptools import setup
+
+setup(name='api-example',
+    version='1.1',
+    py_modules=['example'],
+    author='Munchii',
+    author_email='contact@munchii.me',
+    license='MIT',
+    description='Simple Python API exmaple.',
+    url='https://github.com/Dmunch04/api-example',
+    classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: End Users/Desktop",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+        ],
+    keywords='python example api wrapper',
+    )
